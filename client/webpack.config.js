2bada02ed2ecd32548d421251f83f4b103288e57
MarcoSaverino <marcosaverino@live.ca>
2023-07-26 17:08:30 -0400

    Pushing to Heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,27 +1,11 @@
-// configuring webpack's behavior and building process. It sets the mode, defines entry points, specifies output settings, configures plugins for HTML generation, service worker generation, and PWA manifest generation. It also defines rules for processing different types of files, such as images, CSS, and JavaScript, using loaders. The exported configuration object is used by webpack to build the application
-
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //simplifies the creation of HTML files that serve as entry points
-const WebpackPwaManifest = require('webpack-pwa-manifest'); // generates a web app manifest file
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// Add and configure workbox plugins for a service worker and manifest file.
-
-
 module.exports = () => {
   return {
-    entry: './client/src/js/index.js',
-    output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      // Add necessary rules for handling JavaScript, CSS, and other assets.
-    ],
-  },
-  devServer: { },
-    mode: 'development', //enables additional development-specific features like source maps and readable output
+    mode: 'development',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
@@ -31,15 +15,15 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      new HtmlWebpackPlugin({ //used to generate HTML files for the application. It takes a template file (index.html) and injects the necessary script tags for the bundled JavaScript files
+      new HtmlWebpackPlugin({ 
         template: './index.html',
         title: 'Webpack plugin'
       }),
-      new InjectManifest({ //provided by the Workbox library. It generates a service worker file based on the specified source file (src-sw.js) and destination (sw.js). The service worker is responsible for caching assets and enabling offline functionality (So here it is happening the RIA: Registration, Installation and Activation)
+      new InjectManifest({ 
         swSrc: './src/src-sw.js',
         swDest: 'sw.js'
       }),
-      new WebpackPwaManifest({ //used to generate a web app manifest file (manifest.json) for the PWA. It allows you to specify various properties such as the app's name, description, icons, colors, and starting URL. The manifest file will be used by browsers and platforms to understand and treat the web app as an installable PWA. It enables features like adding the app to the home screen, launching it in a standalone window, and controlling how the app appears when launched
+      new WebpackPwaManifest({ 
         name: 'JATE(Text-editor)',
         short_name: 'JATE',
         description: 'Just Another Text Editor',
@@ -62,28 +46,24 @@ module.exports = () => {
         ],
       })
     ],
-
-
-// Add CSS loaders and babel to webpack.
-
-    module: { // module property defines rules for transforming and processing different types of files in the application
+    module: {
       rules: [
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i, //first rule handles image files
-          type: 'asset/resource' //the matched image files will be emitted as separate files in the output directory and their URLs will be added to the JavaScript code dynamically
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource'
         },
         {
-          test: /\.css$/i, //second rule handles CSS files
-          use: ['style-loader', 'css-loader'], // the use property is an array of loaders that will be applied in reverse order. In this case, the 'css-loader' is used to handle CSS imports, and the 'style-loader' is used to inject the CSS into the HTML as <style> tags
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
         },
         {
-          test: /\.m?js$/, // third rule handles JavaScript files 
+          test: /\.m?js$/,
           exclude: /node_modules/,
-          use: { // specifies the loader to be used for JavaScript: babel-loader
-            loader: 'babel-loader', // to make sure that the code is readable by all browsers, we need a tool like babel to transpile our code to normal readable code for browsers
+          use: {
+            loader: 'babel-loader',
             options: {
               presets: [
-                ['@babel/preset-env', { targets: "defaults" }] //
+                ['@babel/preset-env', { targets: "defaults" }]
               ]
             }
           }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Serve static files from the "client/dist" directory
+app.use('/dist', express.static(path.join(__dirname, '../client/dist')));
 
-require('./routes/htmlRoutes')(app);
+// If you're using a single page app routing
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+app.listen(3000);
